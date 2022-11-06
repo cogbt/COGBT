326382cc10d8dc33776708b0c77fc8c830470018
qwqcxh <qwqcxh@gmail.com>
2022-11-06 15:03:38 +0800

    feat: translate test

--- accel/cogbt/translator/X86/misc.cpp
+++ accel/cogbt/translator/X86/misc.cpp
@@ -2855,8 +2855,11 @@ void X86Translator::translate_t1mskc(GuestInst *Inst) {
     exit(-1);
 }
 void X86Translator::translate_test(GuestInst *Inst) {
-    dbgs() << "Untranslated instruction test\n";
-    exit(-1);
+    X86InstHandler InstHdl(Inst);
+    Value *Src0 = LoadOperand(InstHdl.getOpnd(1));
+    Value *Src1 = LoadOperand(InstHdl.getOpnd(0));
+    Value *Dest = Builder.CreateAnd(Src0, Src1);
+    CalcEflag(Inst, Dest, Src0, Src1);
 }
 void X86Translator::translate_ud2(GuestInst *Inst) {
     dbgs() << "Untranslated instruction ud2\n";
