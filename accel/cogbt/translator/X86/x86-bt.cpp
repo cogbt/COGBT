5befe8de4b463f074473fe8a309020a486e7a3b9
qwqcxh <1686157273@qq.com>
2023-01-31 17:33:27 +0800

    fix bt llvm type

--- accel/cogbt/translator/X86/x86-bt.cpp
+++ accel/cogbt/translator/X86/x86-bt.cpp
@@ -28,14 +28,21 @@ void X86Translator::translate_bt(GuestInst *Inst) {
         Value *index = nullptr;
         Value *base = CalcMemAddr(InstHdl.getOpnd(1));
         if (Opnd0Hdl.isImm()) {
-            index = ConstInt(OpndTy, Opnd0Hdl.getIMM() % (OpndSize << 3));
+            index = ConstInt(Int8Ty, Opnd0Hdl.getIMM() % (OpndSize << 3));
+            if (Opnd0Hdl.getIMM() >> 3) {
+                Value *extraBytes = ConstInt(Int64Ty, Opnd0Hdl.getIMM() >> 3);
+                base = Builder.CreateAdd(base, extraBytes);
+            }
         } else { // index is reg
-            index = LoadOperand(InstHdl.getOpnd(0));
-            Value *extraBytes = Builder.CreateAShr(index, ConstInt(OpndTy, 3));
-            index = Builder.CreateAnd(index, ConstInt(OpndTy, 7));
+            index = LoadOperand(InstHdl.getOpnd(0), Int64Ty);
+            Value *extraBytes = Builder.CreateAShr(index, ConstInt(Int64Ty, 3));
             base = Builder.CreateAdd(base, extraBytes);
+            index = Builder.CreateAnd(index, ConstInt(Int64Ty, 7));
+            index = Builder.CreateTrunc(index, Int8Ty);
         }
-        base = Builder.CreateLoad(Int8Ty, base);
+        // base is int64ty and index is int8ty now.
+        base =
+            Builder.CreateLoad(Int8Ty, Builder.CreateIntToPtr(base, Int8PtrTy));
         base = Builder.CreateLShr(base, index);
         base = Builder.CreateAnd(base, ConstInt(Int8Ty, 1));
         base = Builder.CreateZExt(base, Int64Ty);
